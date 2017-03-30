8bac580564aec12f86d3c9c7cbeaf77df7ac8082
snipzer <l.arif@laposte.net>
2017-03-30 15:13:57 +0200

    Clean de la sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,29 +1,3 @@
 <div class="sidebar">
 
-    <ul>
-
-        <?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar() ) : else : ?>
-
-            <?php wp_list_pages('depth=3&title_li=<h2>Pages</h2>'); ?>
-
-            <li><h2><?php _e('Categories'); ?></h2>
-                <ul>
-                    <?php wp_list_categories('sort_column=name&optioncount=1&hierarchical=0'); ?>
-                </ul>
-            </li>
-
-            <li><h2><?php _e('Archives'); ?></h2>
-                <ul>
-                    <?php wp_get_archives('type=monthly'); ?>
-                </ul>
-            </li>
-
-            <?php wp_list_bookmarks(); ?>
-
-
-
-        <?php endif; ?>
-
-    </ul>
-
 </div>
