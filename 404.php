89298696a8a0c11da4595337db9c499de762b40a
PochWar <ripoche.b@gmail.com>
2017-03-30 14:08:48 +0200

    edit 404.php

--- 404.php
+++ 404.php
@@ -1,8 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: snipzer
- * Date: 30/03/17
- * Time: 10:01
- */
-?>
+<?php get_header(); ?>
+
+<div id="container">
+
+    <section class="error-404 not-found">
+        <header class="page-header">
+            <h1 class="page-title">I AM 404 !</h1>
+        </header><!-- .page-header -->
+        <div class="page-content">
+            <div id="error">
+            </div>
+            <p>Apparemment, rien n’a été trouvé à cette adresse. Essayez avec une recherche ?</p>
+            <?php get_search_form(); ?>
+        </div><!-- .page-content -->
+    </section><!-- .error-404 -->
+
+</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
