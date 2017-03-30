7a9af9d7978eab1afb3de2c5dc34f9d2922ac70b
JeanPoulard <jean.poulardbore@gmail.com>
2017-03-30 12:01:32 +0200

    la vie est cool (modif footer)

--- footer.php
+++ footer.php
@@ -2,6 +2,10 @@
     <p>
         Copyright 2007 <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
     </p>
+    <p>
+        Site créé par : snipzer, pochwar, Aliz, LokiHash
+    </p>
+
 </div>
 
 </div></body>
