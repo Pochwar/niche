e04d8e74e2fa9f978d0c8cd7e1ef57e8f9ded26f
PochWar <ripoche.b@gmail.com>
2017-03-31 10:10:11 +0200

    maj template et css pour sidebar

--- page.php
+++ page.php
@@ -1,26 +1,27 @@
 <?php get_header(); ?>
-
+<div id="content">
     <div id="box">
 
-<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+        <?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
 
-    <div class="post">
+            <div class="post">
 
-        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 
-        <div class="entry">
+                <div class="entry">
 
-            <?php the_content(); ?>
+                    <?php the_content(); ?>
 
 
-        </div>
-    </div>
+                </div>
+            </div>
 
-<?php endwhile; ?>
+        <?php endwhile; ?>
 
     </div>
 <?php endif; ?>
 
 
 <?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
