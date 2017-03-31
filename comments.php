e00571e64b327c9e74af55745137e8b649ada670
snipzer <l.arif@laposte.net>
2017-03-31 13:56:45 +0200

    Traduction en français

--- comments.php
+++ comments.php
@@ -1,13 +1,13 @@
 <?php
 if ('comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
-    die ('Please do not load this page directly. Thanks!');
+    die ('Ne pas charger cette page directement, merci !');
 if (!empty($post->post_password)) { // if there's a password
     if ($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password)
     {  // and it doesn't match the cookie
         ?>
 
-        <h2><?php _e('Password Protected'); ?></h2>
-        <p><?php _e('Enter the password to view comments.'); ?></p>
+        <h2><?php _e('Mot de passe proteger'); ?></h2>
+        <p><?php _e('Entrer un mot de passe pour voir les commentaires.'); ?></p>
 
         <?php return;
     }
@@ -22,8 +22,8 @@ $oddcomment = 'alt';
 
 
 <?php if ($comments) : ?>
-    <h3 id="comments"><?php comments_number('No Responses', 'One Response', '% Responses' );?>
-        to &#8220;<?php the_title(); ?>&#8221;</h3>
+    <h3 id="comments"><?php comments_number('Pas de réponse', 'Une réponse', '% réponses' );?>
+        vers &#8220;<?php the_title(); ?>&#8221;</h3>
 
     <ol class="commentlist">
         <?php foreach ($comments as $comment) : ?>
@@ -31,13 +31,13 @@ $oddcomment = 'alt';
             <li class="<?php echo $oddcomment; ?>" id="comment-<?php comment_ID() ?>">
 
                 <div class="commentmetadata">
-                    <strong><?php comment_author_link() ?></strong>, <?php _e('on'); ?>
+                    <strong><?php comment_author_link() ?></strong>, <?php _e('le'); ?>
                     <a href="#comment-<?php comment_ID() ?>"
-                       title=""><?php comment_date('F jS, Y') ?> <?php _e('at');?>
-                        <?php comment_time() ?></a> <?php _e('Said&#58;'); ?>
-                    <?php edit_comment_link('Edit Comment','',''); ?>
+                       title=""><?php comment_date('F jS, Y') ?> <?php _e('à');?>
+                        <?php comment_time() ?></a> <?php _e('Dit&#58;'); ?>
+                    <?php edit_comment_link('Editer le commentaire','',''); ?>
                     <?php if ($comment->comment_approved == '0') : ?>
-                        <em><?php _e('Your comment is awaiting moderation.'); ?></em>
+                        <em><?php _e('Votre commentaire est en attente de modération'); ?></em>
                     <?php endif; ?>
                 </div>
 
@@ -59,7 +59,7 @@ $oddcomment = 'alt';
     <?php else : // comments are closed ?>
 
 
-        <p class="nocomments">Comments are closed.</p>
+        <p class="nocomments">Les commentaires sont fermés</p>
 
     <?php endif; ?>
 <?php endif; ?>
@@ -67,12 +67,12 @@ $oddcomment = 'alt';
 
 <?php if ('open' == $post->comment_status) : ?>
 
-    <h3 id="respond">Leave a Reply</h3>
+    <h3 id="respond">Laisser un commentaire</h3>
 
     <?php if ( get_option('comment_registration') && !$user_ID ) : ?>
         <p>You must be <a href="<?php echo get_option('siteurl'); ?>
 /wp-login.php?redirect_to=<?php the_permalink(); ?>">
-                logged in</a> to post a comment.</p>
+                connecter</a> pour envoyer un commentaire.</p>
 
     <?php else : ?>
 
@@ -80,34 +80,30 @@ $oddcomment = 'alt';
 /wp-comments-post.php" method="post" id="commentform">
             <?php if ( $user_ID ) : ?>
 
-                <p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php">
+                <p>Connecter en tant que <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php">
                         <?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>
 /wp-login.php?action=logout"
-                                                              title="Log out of this account">Logout &raquo;</a></p>
+                                                              title="Log out of this account">Deconnexion &raquo;</a></p>
 
             <?php else : ?>
 
                 <p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>"
                           size="40" tabindex="1" />
-                    <label for="author"><small>Name <?php if ($req) echo "(required)"; ?></small></label></p>
+                    <label for="author"><small>Nom <?php if ($req) echo "(requis)"; ?></small></label></p>
 
                 <p><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>"
                           size="40" tabindex="2" />
-                    <label for="email"><small>Mail (will not be published) <?php if ($req) echo "(required)"; ?>
+                    <label for="email"><small>Mail (Ne sera pas publier) <?php if ($req) echo "(requis)"; ?>
                         </small></label></p>
-
-                <p><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>"
-                          size="40" tabindex="3" />
-                    <label for="url"><small>Website</small></label></p>
 
             <?php endif; ?>
 
-            <!--<p><small><strong>XHTML:</strong> <?php _e('You can use these tags&#58;'); ?>
+            <!--<p><small><strong>XHTML:</strong> <?php _e('Vous pouvez utiliser ces tag &#58;'); ?>
 <?php echo allowed_tags(); ?></small></p>-->
 
             <p><textarea name="comment" id="comment" cols="60" rows="10" tabindex="4"></textarea></p>
 
-            <p><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
+            <p><input name="submit" type="submit" id="submit" tabindex="5" value="Envoyer !" />
                 <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
             </p>
 
--- index.php
+++ index.php
@@ -14,9 +14,9 @@
                         <?php the_content(); ?>
 
                         <p class="postmetadata">
-                            <?php _e('Filed under&#58;'); ?> <?php the_category(', ') ?> <?php _e('by'); ?>
+                            <?php the_title() ?> <?php _e(', by'); ?>
                             <?php  the_author(); ?><br />
-                            <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
+                            <?php comments_popup_link('Aucun commentaire &#187;', '1 Commentaire &#187;', '% Commentaires &#187;'); ?>
                             <?php edit_post_link('Edit', ' &#124; ', ''); ?>
                         </p>
 
